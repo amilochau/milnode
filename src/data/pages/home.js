c34c75ed6d75ae82be815dd6ce6e26a82d4f9fb2
Antoine Milochau <amilochau@outlook.com>
2019-04-02 17:59:03 +0200

    Do not display all home pages shortcuts

--- src/data/pages/home.js
+++ src/data/pages/home.js
@@ -1,6 +1,10 @@
-export default [
-  { to: { name: 'home/Help' }, icon: 'help', title: 'pages.home.help.title' },
-  { to: { name: 'home/Feedback' }, icon: 'feedback', title: 'pages.home.feedback.title' },
+import Vue from 'vue'
+
+const homePages = [
+  { to: { name: 'home/Help' }, icon: 'help', title: 'pages.home.help.title', options: { backRequired: true } },
+  { to: { name: 'home/Feedback' }, icon: 'feedback', title: 'pages.home.feedback.title', options: { backRequired: true } },
   { to: { name: 'home/Privacy' }, icon: 'gavel', title: 'pages.home.privacy.title' },
-  { to: { name: 'home/About' }, icon: 'copyright', title: 'pages.home.about.title' }
-]
+  { to: { name: 'home/About' }, icon: 'copyright', title: 'pages.home.about.title', options: { backRequired: true } }
+].filter(r => !Vue.prototype.$noBack || !(r.options && r.options.backRequired))
+
+export default homePages
